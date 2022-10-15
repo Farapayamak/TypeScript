e5c30dc4827341739374e333cc7efe7d03b9af55
amhome <amirhosseinmehrvarzi@gmail.com>
2022-10-15 17:18:01 +0330

    rest type definitions

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+// type definitions for Farapayamk
+
+export class RestClient {
+
+  loggingEnable: boolean;
+
+  constructor(username: string, password: string)
+
+  SendSMS = (to: string, from: string, text: string, isFlash: boolean = false) => Promise;
+  GetDeliveries = (recId: number) => Promise;
+  GetMessages = (location: number, from: string, index: number, count: numebr) => Promise;
+  GetCredit = () => Promise;
+  GetBasePrice = () => Promise;
+  GetUserNumbers = () => Promise;
+  BaseServicenumber = (text: string, to: string, bodyId: number) => Promise;
+}
+
