10765afccab02a25fd2021b37e6f6bec0af35732
amhome <amirhosseinmehrvarzi@gmail.com>
2022-10-22 13:52:06 +0330

    v1.2.0

---
+++ example.ts
@@ -0,0 +1,17 @@
+// Use the following import after adding this package to your project dependencies
+// import { RestClient, SoapClient } from 'farapayamak-ts';
+
+
+
+// var rest_cl = new RestClient("username", "password");
+// rest_cl.SendSMS("09123456789", "5000xxx", "test sms")
+//   .then(res => console.log(res));
+
+// var soap_cl = new SoapClient("username", "password");
+// soap_cl.SendSimpleSMS2("09123456789", "5000xxx", "test sms", false)
+//   .then(res => console.log(res));
+
+// soap_cl.GetDeliveries({long: [123456789, 123456789]})
+//   .then(res => console.log(res));
+// soap_cl.SendSimpleSMS({string: ["09123456789", "09123456789"]}, "5000xxx", "test sms", false)
+//   .then(res => console.log(res));
